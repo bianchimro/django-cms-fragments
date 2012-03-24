fc78f93a496e55590ad2bf70bd1685cf3768aefb
Mauro Bianchi <bianchimro@gmail.com>
2012-03-24 16:23:06 +0100

    fixed typo

--- cms_fragments/__init__.py
+++ cms_fragments/__init__.py
@@ -1,5 +1,5 @@
 __version__ = '0.0.3'
 __author__ = 'Mauro Bianchi'
-__email__ = "bianchimro@gmail.com'
+__email__ = 'bianchimro@gmail.com'
 __url__ = 'https://github.com/bianchimro/django-cms-fragments'
 
